da362c3ed36667f420cca6ec200b7216c81ca5c3
jacqueline-homan <jacquelinehoman7@gmail.com>
2017-02-03 18:08:13 -0500

    refactoring attempt on AT8

--- AT8.fs
+++ AT8.fs
@@ -31,7 +31,7 @@ type LadingQty =
     | LadingQty of string
 
 let plqty : Parser<LadingQty option> = 
-    optfield' (pValue 1 7) LadingQty
+    optfield' (manyMinMaxSatisfy 1 7 isDigit) LadingQty 
 
 type AT8 = AT8 of WtQual * WtCode * Weight * LadingQty option
 
